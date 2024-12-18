5045e6593fa30d8c4f6519401465cfee561c1688
mxnuchim <manuchimoliver779@gmail.com>
2024-12-18 19:25:05 +0100

    fix

--- components/modal/CenterModal.tsx
+++ components/modal/CenterModal.tsx
@@ -1,5 +1,5 @@
 // components/CenterModal.tsx
-import React, { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 
 interface CenterModalProps {
   isOpen: boolean;
@@ -26,33 +26,35 @@ const CenterModal: React.FC<CenterModalProps> = ({
   }, [isOpen, onClose]);
 
   return (
-    <div
-      className={`fixed inset-0 z-50 flex items-center justify-center transition-opacity duration-300 ${
-        isOpen ? "visible opacity-100" : "invisible opacity-0"
-      } `}
-    >
-      {/* Overlay */}
+    <Suspense>
       <div
-        className="absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-300"
-        onClick={onClose}
-      />
-
-      {/* Modal Content */}
-      <div
-        className={`relative z-10 max-w-lg w-full bg-white rounded-lg shadow-lg p-6 transform transition-transform duration-300 ${
-          isOpen ? "scale-100" : "scale-95"
-        } ${className}`}
+        className={`fixed inset-0 z-50 flex items-center justify-center transition-opacity duration-300 ${
+          isOpen ? "visible opacity-100" : "invisible opacity-0"
+        } `}
       >
-        {/* <button
+        {/* Overlay */}
+        <div
+          className="absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-300"
+          onClick={onClose}
+        />
+
+        {/* Modal Content */}
+        <div
+          className={`relative z-10 max-w-lg w-full bg-white rounded-lg shadow-lg p-6 transform transition-transform duration-300 ${
+            isOpen ? "scale-100" : "scale-95"
+          } ${className}`}
+        >
+          {/* <button
           className="absolute top-4 right-4 text-gray-600 hover:text-gray-900 focus:outline-none"
           onClick={onClose}
           aria-label="Close"
         >
           ✕
         </button> */}
-        {children}
+          {children}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
